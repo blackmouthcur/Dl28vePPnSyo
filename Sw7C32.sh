a839d5775c9c5a06a0c05374a32639a725fe11c2
blackmouthcur <xcurtisx@gmail.com>
2021-08-05 16:49:08 -0400

    Update Sw7C32.sh

--- Sw7C32.sh
+++ Sw7C32.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-#disable SELINUX in /etc/selinx/conf
 #Run this from shell at root: curl https://raw.githubusercontent.com/blackmouthcur/Dl28vePPnSyo/main/Sw7C32.sh | bash
 echo "------------------------------------------------"
 echo "			CENTOS 8 Plex and Samba Set Up"
 echo "------------------------------------------------"
-echo "Checking for updates"
+echo "Changing SELINUX default to disabled"
+cp /etc/selinux/conf /etc/selinux/conf.bak
+echo "Made /etc/selinux/conf.bak"
+find /etc/selinux/conf -type f -name 'conf' -exec sed -i 's/enabled/disabled/g' {} \;
+echo "Done"
+echo " "
+echo "Checking for CENTOS updates / upgrades"
 dnf update
 dnf upgrade
 echo " " 
+sleep 3
+clear
 echo "Downloading and Installing Samba and Samba Client"
 sudo yum install samba samba-client -y
 echo "Done"
@@ -28,13 +35,16 @@ cd /etc/samba
 wget https://raw.githubusercontent.com/blackmouthcur/Dl28vePPnSyo/main/smb.conf
 echo "Samba Installed and two shares created."
 echo " "
-sleep 3
 echo " Moving to PLEX Install"
+sleep 3
+clear
 echo "Getting plex repo from git repo"
 cd /etc/yum.repos.d/
 wget https://raw.githubusercontent.com/blackmouthcur/Dl28vePPnSyo/main/plex_media.repo
 dnf install plexmediaserver -y
-echo " "
+echo " Moving to Firewall"
+sleep 3
+clear
 echo "Setting Firewall Rules for PLEX and SAMBA"
 firewall-cmd --permanent --zone=public --add-service=samba
 firewall-cmd --zone=public --add-service=samba
@@ -42,16 +52,21 @@ firewall-cmd --add-service=plex --zone=public --permanent
 firewall-cmd --reload
 echo " Firewall Reloaded and Done"
 echo " "
+echo " Enabling Services"
+sleep 3
+clear
 systemctl enable plexmediaserver
 systemctl start plexmediaserver
 sudo systemctl start smb.service
 sudo systemctl start nmb.service
 sudo systemctl enable smb.service
 sudo systemctl enable nmb.service
-echo " "
+sleep 3
+clear
 echo "Plex is at: http://server-ip:32400/web hit it up"
 echo "Samba: \\serverip or dns name\ (File Explorer) "
 echo " "
 echo "------------------------------------------------"
 echo "               SCRIPT COMPLETE"
 echo "------------------------------------------------"
+
